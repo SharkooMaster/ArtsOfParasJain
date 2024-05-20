3c94e04f9acd583711d0311c933644579e7f81ee
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2024-05-20 03:12:22 +0200

    Auto "db"

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
+class portfolioPiece {
+	constructor(title, _id) {
+		this.title = title.slice(0, -1).replaceAll("_", " ");
+		this._id = _id;
 
-// for each folder in path.
-//	- Create card based on folder name.
-//	- Populate card popup with images from within folder.
-
-class portfolioPiece
-{
-	constructor(title) {
-		this.title = title.slice(0, -1);
-		this.instantiate();
+		this.content = [];
 	}
 
-	instantiate()
-	{
+	instantiate() {
 		var portfolio_container = document.getElementById("portfolio_container");
-		portfolio_container.innerHTML += "<title_card title=" + this.title + "></title_card>";
+		portfolio_container.innerHTML += "<title_card content='" + JSON.stringify(this.content) + "' pid='" + this._id + "' title='" + this.title + "'></title_card>";
 	}
 }
 
@@ -30,10 +25,35 @@ function loadFolders() {
 			var links = doc.querySelectorAll('a');
 			links.forEach(function(link) {
 				var href = link.getAttribute('href');
-				pieces.push(new portfolioPiece(href));
+				var piece = new portfolioPiece(href, pieces.length);
+				pieces.push(piece);
+				loadFilesInFolder("portfolio/" + href, piece);
 			});
 		} else {
 			console.error("Failed to load directory listing. Status:", xhr.status);
+		}
+	};
+	xhr.onerror = function() {
+		console.error("Request failed.");
+	};
+	xhr.send();
+}
+
+function loadFilesInFolder(folderPath, piece) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', folderPath, true);
+	xhr.onload = function() {
+		if (xhr.status === 200) {
+			var parser = new DOMParser();
+			var doc = parser.parseFromString(xhr.responseText, 'text/html');
+			var links = doc.querySelectorAll('a');
+			links.forEach(function(link) {
+				var href = link.getAttribute('href');
+				piece.content.push(href);
+			});
+			piece.instantiate(); // Instantiate the piece only after content is loaded
+		} else {
+			console.error("Failed to load folder contents. Status:", xhr.status);
 		}
 	};
 	xhr.onerror = function() {
